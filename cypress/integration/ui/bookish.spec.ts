e392df8a3082c839864fa8b0623a06fdc09744af
Ahmed Nouira <ahmnouira@gmail.com>
2021-07-24 02:49:14 +0100

    feat: config for cypress typescript

---
+++ cypress/integration/ui/bookish.spec.ts
@@ -0,0 +1,5 @@
+describe('', () => {
+  it('Visits the booksih', () => {
+    cy.visit('')
+  })
+})
---
+++ cypress/plugins/index.ts
@@ -0,0 +1,8 @@
+// cypress/plugins/index.ts
+
+/// <reference types="cypress" />
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (_on: any, _config: any) => {}
